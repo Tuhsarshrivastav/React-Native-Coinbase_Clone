45c63a6c642a249f5c6f60982ecf2bd794ad193b
Tusharshrivastav <ts3657@gmail.com>
2021-06-15 14:21:10 +0530

    build complete

---
+++ components/Transfer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Transfer = () => {
+  return (
+    <View>
+      <Text>Transfer</Text>
+    </View>
+  );
+};
+
+export default Transfer;
